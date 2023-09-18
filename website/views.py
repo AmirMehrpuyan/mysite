7e9fb843d037883e686a69cfce7a7f2155d44688
AmirMehrpuyan <ah.mehrpuyan@gmail.com>
2023-09-18 12:25:48 +0330

    create new app and change templates
    create blog app
    change index, about and contact
    blog-home and blog-single

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index_view(requests):
-    return render(requests,'index.html')
+    return render(requests,'website/index.html')
 def about_view(requests):
-    return render(requests,'about.html')
+    return render(requests,'website/about.html')
 def contact_view(requests):
-    return render(requests,'contact.html')
+    return render(requests,'website/contact.html')
 # Create your views here.
